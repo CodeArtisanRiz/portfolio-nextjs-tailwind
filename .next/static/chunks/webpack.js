e726db174375d098eab6f6095c2e019de37222dc
H M Rizwan Mazumder <42119262+xtremist96@users.noreply.github.com>
2022-10-04 12:19:13 +0530

    phase 1 complete
    Basic Design
    darkmode
    responsive

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "616b1ba15c740674"; }
+/******/ 		__webpack_require__.h = function() { return "21a8b1bdec5b3b72"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
